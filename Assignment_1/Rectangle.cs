0138f0e74d732c80bff479a2b9bf695f05065250
Vidhi Patel <vidhibpatel94@gmail.com>
2021-02-09 15:19:13 -0500

    Completed Rectangle class

---
+++ Assignment_1/Rectangle.cs
@@ -0,0 +1,51 @@
+﻿using System;
+namespace Assignment_1
+{
+    public class Rectangle
+    {
+        private int Length;
+        private int Width;
+
+        public Rectangle()
+        {
+            this.Length = 1;
+            this.Width = 1;
+        }
+
+        public Rectangle(int length, int width)
+        {
+            this.Length = length;
+            this.Width = width;
+        }
+
+        public int GetLength()
+        {
+            return Length;
+        }
+
+        public void SetLength(int length)
+        {
+            this.Length = length;
+        }
+
+        public int GetWidth()
+        {
+            return Width;
+        }
+
+        public void SetWidth(int width)
+        {
+            this.Width = width;
+        }
+
+        public int GetPerimeter()
+        {
+            return 2 * (Length + Width);
+        }
+
+        public int GetArea()
+        {
+            return Length * Width;
+        }
+    }
+}
